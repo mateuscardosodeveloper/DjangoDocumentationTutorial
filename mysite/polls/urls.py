ffc98d04ba4e534bfd4def32c0c87999f8c88449
raiizoor <mateus.informatica44@gmail.com>
2021-05-11 21:14:45 -0300

    Finished tutorial 5 - Testing

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,6 +4,7 @@ from polls import views
 
 
 app_name='polls'
+
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail' ),
